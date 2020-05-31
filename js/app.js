040fd6a06bd2e12aa15aa7ce2e6d499436abb62e
unknown <nikiforiakoleksandr@gmail.com>
2020-06-01 01:24:18 +0300

    'require-fix-bugs-js'

--- js/app.js
+++ js/app.js
@@ -5,15 +5,39 @@ $(function(){
     const operators=['+', '-', '*', '/'];
 
     const operator =operators[Math.floor(Math.random()*operators.length)]
-
+    const buttons=$('.btn');
+    console.log(buttons)
+    const btnArray=[];
+    let question;
     let result;
-
-    operator==='/' || operator==='-'? result=`${Math.max(a,b)} ${operator} ${Math.min(a,b)} =` : result=`${a} ${operator} ${b} =`
+    
+    //fix min-max num
+    operator==='/' || operator==='-'? question=`${Math.max(a,b)} ${operator} ${Math.min(a,b)} =` : question=`${a} ${operator} ${b} =`
         
+    //results
+   switch (operator){
+       case '+': result=a+b;
+       break;
+       case '-': result=Math.max(a,b) - Math.min(a,b);
+       break;
+       case '/': result=(Math.max(a,b) / Math.min(a,b));
+       break;
+       case '*': result=a*b;
+       break;
+   }
+
+ 
+//Problem
+    let innerBtn=[(result), (result-1), result+1, result+2]
+
+    buttons.forEach(el=>{
+        return el.text(innerBtn.forEach(item=>{
+            item(splice(Math.floor(Math.random()*innerBtn.length)))
+        }))
+    })
     
 
-
-    console.log(result)
+       console.log(btnArray)
 
 
 
