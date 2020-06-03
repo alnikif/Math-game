24e99279b73d98b8d781cf091e5f014d5ddd8a9c
unknown <nikiforiakoleksandr@gmail.com>
2020-06-03 15:28:28 +0300

    'add-some-logic'

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 
   
     let a,b;
-    const operators=['+', '-', '*', '/'];
+    const operators=['/', '-', '*', '+'];
 
-    const operator =operators[Math.floor(Math.random()*operators.length)]
+    let operator;
     const buttons=document.querySelectorAll('.btn');
     const resultText=document.querySelector('.result')
     const btnArray=[];
@@ -14,12 +14,32 @@
     let counter=10;
     let questionText;
     let result;
-    let innerBtn=[];
-   
-    
 
-    //shufle function
-    function shuffle(arr){
+//add start-event 
+const pressStart=()=>{
+        //choose operator
+        operator =operators[Math.floor(Math.random()*operators.length)]
+     //create random numbers
+    a= Math.floor(Math.random()*10)+1;
+    b= Math.floor(Math.random()*10)+1;
+    //fix min-max num
+    operator==='/' || operator==='-'? questionText=`${Math.max(a,b)} ${operator} ${Math.min(a,b)} =` : questionText=`${a} ${operator} ${b} =`
+    let innerBtn=[];
+    //results
+    switch (operator){
+        case '+': result=a+b;
+        break;
+        case '-': result=Number(Math.max(a,b) - Math.min(a,b));
+        break;
+        case '/': result=Number(Math.max(a,b) / Math.min(a,b));
+        break;
+        case '*': result=a*b;
+        break;
+    }
+
+    innerBtn.push(result, result+1, result+2, result-1);
+     //shufle function
+     function shuffle(arr){
         var j, temp;
         for(var i = arr.length - 1; i > 0; i--){
             j = Math.floor(Math.random()*(i + 1));
@@ -30,69 +50,38 @@
         return arr;
     }
     shuffle(innerBtn);
-
-   
-  
-
-
-    //add check result-event
-    const checkResult=e=>{
-        if(e.target.innerText==result){
-            question.innerHTML='Gratuluje!';
-            clearInterval(interval);
-            
-        }else {
-            question.innerHTML='Błędna odpowiedź!';
-            clearInterval(interval);
-        }
+     //add innerHTML for buttons
+    for(let i = 0; i<buttons.length; i++){
+        buttons[i].innerHTML=innerBtn[i];
+        buttons[i].style.display='inline';
     }
-    buttons.forEach(el=>{
-        el.addEventListener('click', checkResult )     
-    })
-
-    //add start-event 
-   
-    const pressStart=()=>{
-        //create random numbers
-        a= Math.floor(Math.random()*10)+1;
-        b= Math.floor(Math.random()*10)+1;
-        //fix min-max num
-    operator==='/' || operator==='-'? questionText=`${Math.max(a,b)} ${operator} ${Math.min(a,b)} =` : questionText=`${a} ${operator} ${b} =`
-        
-    //results
-   switch (operator){
-    case '+': result=a+b;
-    break;
-    case '-': result=Math.max(a,b) - Math.min(a,b);
-    break;
-    case '/': result=(Math.max(a,b) / Math.min(a,b));
-    break;
-    case '*': result=a*b;
-    break;
-    }
-    innerBtn.push(result, result+1, result+2, result-1)
-         //add innerHTML for buttons
-        for(let i = 0; i<buttons.length; i++){
-            buttons[i].innerHTML=innerBtn[i];
-            buttons[i].style.display='inline';
-        }
         question.innerHTML=questionText;
-        //add timer
-        counter=10
-         question.style.display='inline';
-            interval=setInterval(()=>{
-            counter-=1
-            timer.innerHTML=counter;
-
-            if(counter<1){
-                clearInterval(interval);
-                question.innerHTML='Time is over';
-
+     //add timer
+    counter=10
+    question.style.display='inline';
+        interval=setInterval(()=>{
+        counter-=1
+        timer.innerHTML=counter;
+        if(counter<1){
+            clearInterval(interval);
+            question.innerHTML='Time is over';
             }
         }, 1000)
-
-    }
+}
     start.addEventListener('click', pressStart)
-
+ //add check result-event
+const checkResult=e=>{
+    if(e.target.innerText==result){
+        question.innerHTML='Gratuluje!';
+        clearInterval(interval);
+        
+    }else {
+        question.innerHTML='Błędna odpowiedź!';
+        clearInterval(interval);
+    }
+}
+buttons.forEach(el=>{
+    el.addEventListener('click', checkResult )     
+})
 
 
